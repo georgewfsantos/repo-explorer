df7265f802d3aa7ef9555be3ad21b0aa48d8ab1f
George W F Santos <gweverton21@hotmail.com>
2020-06-09 19:26:58 -0300

    finish styling Dashboard page

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,13 +1,60 @@
 import React from 'react';
+import { FiChevronRight } from 'react-icons/fi';
 
 import logo from '../../assets/logo.svg';
-import { Title } from './styles';
+import { Title, Form, Repositories } from './styles';
 
 const Dashboard: React.FC = () => {
   return (
     <>
       <img src={logo} alt="Github" />
       <Title>Explore repositórios no Github</Title>
+
+      <Form>
+        <input placeholder="Digite o nome do repositório" />
+        <button type="submit">Pesquisar</button>
+      </Form>
+
+      <Repositories>
+        <a href="teste">
+          <img
+            src="https://greendestinations.org/wp-content/uploads/2019/05/avatar-exemple.jpg"
+            alt="Someone"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS and React Native forms</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://greendestinations.org/wp-content/uploads/2019/05/avatar-exemple.jpg"
+            alt="Someone"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS and React Native forms</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+
+        <a href="teste">
+          <img
+            src="https://greendestinations.org/wp-content/uploads/2019/05/avatar-exemple.jpg"
+            alt="Someone"
+          />
+          <div>
+            <strong>rocketseat/unform</strong>
+            <p>Easy peasy highly scalable ReactJS and React Native forms</p>
+          </div>
+
+          <FiChevronRight size={20} />
+        </a>
+      </Repositories>
     </>
   );
 };
