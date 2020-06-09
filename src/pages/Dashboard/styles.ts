a957d8a511867e6a006b893711af17b9a65ae104
George W F Santos <gweverton21@hotmail.com>
2020-06-08 21:24:15 -0300

    set up project and routes

---
+++ src/pages/Dashboard/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+// export const Container = styled.div``;
+
+export const Title = styled.h1`
+  font-size: 48px;
+  color: #3a3a3a;
+  line-height: 56px;
+
+  max-width: 500px;
+`;
